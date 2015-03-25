2ba29fefdf6518d2e6a38761fec435afb5b61127
Warboss-rus <Urgaksh@rambler.ru>
2015-03-25 20:22:04 +0300

    Added list of upgrades

--- src/com/example/wh40k/infoUnit.java
+++ src/com/example/wh40k/infoUnit.java
@@ -6,10 +6,14 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * Created by Dertkaes on 3/25/2015.
@@ -18,12 +22,19 @@ public class infoUnit extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.info);
+        setContentView(R.layout.info_upgrades);
         W40kUnit unit = getIntent().getParcelableExtra("unit");
+        ListView upgrades = (ListView)findViewById(R.id.listView2);
+        View header = getLayoutInflater().inflate(R.layout.info_header, null);
+        upgrades.addHeaderView(header);
         TextView name = (TextView)findViewById(R.id.textView3);
         name.setText(unit.getName());
         TextView description = (TextView)findViewById(R.id.textView4);
         description.setText(unit.getDescription());
+        List<W40kOption> options = unit.getOptions();
+        ArrayAdapter<W40kOption> adapter = new ArrayAdapter<W40kOption>(this, android.R.layout.simple_list_item_1, options);
+        upgrades.setAdapter(adapter);
+        upgrades.requestLayout();
         new DownloadImageTask((ImageView)findViewById(R.id.imageView)).execute(unit.getImagePath());
     }
 
