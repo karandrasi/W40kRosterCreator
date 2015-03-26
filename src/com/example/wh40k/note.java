20bed8241344dc2802ac6cbcb935feec0d7382d9
dertkaes <dertkaes@gmail.com>
2015-03-27 00:31:45 +0300

    note

---
+++ src/com/example/wh40k/note.java
@@ -0,0 +1,15 @@
+package com.example.wh40k;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Dertkaes on 3/26/2015.
+ */
+public class note extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.note);
+    }
+}
